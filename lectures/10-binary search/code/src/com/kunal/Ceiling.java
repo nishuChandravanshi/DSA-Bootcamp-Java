91d5798148ad7b74e1924a7dd832705abb6d0e68
Nishu Chandravanshi <nishu.chandravanshi@Nishu-Chandravanshi-2.local>
2024-02-28 08:02:52 +0530

    u[dated

--- lectures/10-binary search/code/src/com/kunal/Ceiling.java
+++ lectures/10-binary search/code/src/com/kunal/Ceiling.java
@@ -19,7 +19,7 @@ public class Ceiling {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end) {
+        while (start <= end) {
             // find the middle element
 //            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
@@ -33,6 +33,17 @@ public class Ceiling {
                 return mid;
             }
         }
+        /*
+        because, in case of element is not present in the array there are two cases after loop terminates ie in condition start>end
+        last condition before loop exit is start==end==mid
+            1) if target>mid => start=mid+1 => which will lead to start>end condition
+                >in this case it is clear that if target is bigger number then the next number should
+                be the answer, ie new start ie mid+1 itself is the ans.
+            2) if target<mid => end = mid-1; => which will lead to start>end condition
+                >in this case if target is smaller than last mid => the mid itself ie start itself is the ans
+                because mid-1 will be smaller than target, as it is already searched through.
+
+         */
         return start;
     }
 }
